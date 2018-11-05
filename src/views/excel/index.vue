f5408e0d2f8deaa5ddec1c45de110500f99e83cf
王威 <2850208152@qq.com>
2018-11-05 23:26:54 +0800

    添加ECharts

---
+++ src/views/excel/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      list: ''
+    }
+  },
+  methods: {}
+}
+</script>
+<style lang='scss' scoped>
+</style>
