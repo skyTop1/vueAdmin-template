dc1511ea4f9541930ed470e7d55ea839777b7654
王威 <2850208152@qq.com>
2018-12-08 23:39:13 +0800

    修改了一些settings，删除了一些编辑器插件

--- src/views/ext/index.vue
+++ src/views/ext/index.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="ext">
-
+    <el-row :gutter="10">
+      <el-col :span="12">
+        <el-input type="text" v-model="age" @focus="setext"/>
+      </el-col>
+      <el-col :span="12">
+        <el-input type="text" v-model="age" @blur="setext"/>
+        </el-col>
+    </el-row>
   </div>
 </template>
 <script>
 export default {
   name: 'ext',
   data() {
-    return {}
+    return {
+      age: 1
+    }
   },
-  modules: {
+  methods: {
     setext() {
       const o = {}
       console.log(o)
+      console.log(this.age)
     }
   },
-  methods() {
+  mounted() {
     this.setext()
+    console.log(11)
+  },
+  created() {
+    console.log('created')
   }
 }
 </script>
@@ -24,6 +38,8 @@ export default {
 .ext {
   height: 100%;
   background-color: #ccc;
+  min-height: 500px;
+  padding: 10px;
 }
 </style>
 
